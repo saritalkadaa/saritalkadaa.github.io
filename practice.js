1b735bcf0a6598ecea86109b2f7b2f2178e0dc64
unknown <saritkada@icloud.com>
2018-08-02 13:31:51 -0400

    first

---
+++ practice.js
@@ -0,0 +1,18 @@
+// <!-- define a variable -->
+var count=1;
+var name='JULIAN';
+// <!-- loop -->
+for (var i =0;i<10; i++ ) {
+  console.log(i);
+}
+// <!-- make a function -->
+ function add(num1,num2){
+   return num1+num2;
+ }
+ // call a function
+ sum=(1,2
+)
+// conditional
+if (5==5){
+  alert(YAY!)
+}
