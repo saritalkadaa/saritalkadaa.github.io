134d45ccdd5a684fc99bb89b7979b19f3fad9249
saritalkadaa <42039718+saritalkadaa@users.noreply.github.com>
2020-05-08 17:07:49 -0400

    Update practice.js

--- practice.js
+++ practice.js
@@ -3,16 +3,15 @@ var count=1;
 var name='JULIAN';
 // <!-- loop -->
 for (var i =0;i<10; i++ ) {
-  console.log(i);
+  console.log(name + " was called " + i + " times");
 }
 // <!-- make a function -->
  function add(num1,num2){
    return num1+num2;
  }
  // call a function
- sum=(1,2
-)
+ sum= add(1,2);
 // conditional
 if (5==5){
-  alert(YAY!)
+  alert(YAY!);
 }
